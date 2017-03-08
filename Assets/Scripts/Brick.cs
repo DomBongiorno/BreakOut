da732fbc626599797eca7a505427da632ed2f078
Unknown <Dombonge24@yahoo.com>
2017-03-08 12:36:38 -0500

    Added ability for bricks to take more hits

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-
+	int hits = 2;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
+		hits = hits -1;
+
+		if (hits == 0) {
+			Break();
+		}
+	}
+	void Break(){
 		gameObject.SetActive (false);
 	}
 }
